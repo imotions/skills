cf684620fdee7da9fe86e92954fedfc18bb397d7
Bo Gotthardt <mail@gotthardt.bo>
2026-09-17 10:34:17 +0200

    Update imotions-online skill to revision ceeee13f51e2a28a487fb46d8d9e7392bc5bf175

--- skills/imotions-online/aimotions.js
+++ skills/imotions-online/aimotions.js
@@ -35614,7 +35614,7 @@ to input.params.ContentLength in bytes.
 		throw Error("Invalid JSON in IMOTIONS_TEST_REGIONS env var.");
 	}
 	return EMe;
-}, OMe = "11c69b10-4392-4d33-a94b-8279799b5669", kMe = 6e5, t0 = (e) => `${e}-temp`, n0 = () => `${ne.isSea() ? "standalone" : "script"}/2026-09-17-9c7636282`, r0 = async (e, t) => {
+}, OMe = "11c69b10-4392-4d33-a94b-8279799b5669", kMe = 6e5, t0 = (e) => `${e}-temp`, n0 = () => `${ne.isSea() ? "standalone" : "script"}/2026-09-17-ceeee13f5`, r0 = async (e, t) => {
 	let n = O.randomBytes(32).toString("base64url"), r = O.createHash("sha256").update(n).digest("base64url");
 	await g.promises.writeFile(t0(e), JSON.stringify({
 		codeVerifier: n,
